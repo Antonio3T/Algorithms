9767ae09eaf01471ab5243b51ae05a3dba69c50e
António Teixeira <102244893+Antonio3T@users.noreply.github.com>
2022-06-08 17:59:40 +0100

    Add files via upload

---
+++ 20.c
@@ -0,0 +1,192 @@
+//
+//
+//
+//
+//
+//
+//
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//
+//typedef enum _BOOLEAN { FALSE = 0, TRUE = 1 } BOOLEAN;
+//typedef enum STATUS {ERROR = 0, OK = 1} STATUS;
+//
+//#define MAX 50
+//
+//typedef struct Node {
+//	void* Data;
+//	struct Node* next;
+//} NODE;
+//
+//typedef struct ListNode {
+//	void* Data;
+//	struct Node* next;
+//} LISTNODE;
+//
+//typedef LISTNODE* LIST;
+//
+//#define DATA(Node) ((Node)->Data)
+//#define NEXT(Node) ((Node)->next)
+//
+//typedef struct Worker
+//{
+//	char name[MAX], department[MAX];
+//	float paycheck;
+//	BOOLEAN regular;
+//} WORKER;
+//
+//typedef WORKER* LISTWORKER;
+//
+//LISTNODE* NewNode(void*);
+//LISTNODE* DefineBeginning(LIST*, void*);
+//STATUS ReadFile(LIST*, char*);
+//float TotalPaycheck(LIST list);
+//BOOLEAN TurnToRegular(LIST list);
+//void DeleteNonRegular(LIST list);
+//
+//void main()
+//{
+//	LIST list;
+//	WORKER worker;
+//
+//	if (!ReadFile(&list, "Workers.txt"));
+//		return 0;
+//
+//
+//	float total;
+//
+//	//ReadFile(&a, "Workers.txt");
+//	//total = TotalPaycheck(a);
+//	//printf("%.2f", total);
+//
+//	//free(&list);
+//	return 1;
+//}
+//
+//LISTNODE* NewNode(void* Data)
+//{
+//	LISTNODE* NewNode;
+//
+//	if ((NewNode = (LISTNODE*)malloc(sizeof(LISTNODE))) != NULL) {
+//		DATA(NewNode) = Data;
+//		NEXT(NewNode) = NULL;
+//	}
+//	return NewNode;
+//}
+//
+//LISTNODE* DefineBeginning(LIST* list, void* Data)
+//{
+//	LISTNODE* NewNode;
+//
+//	if ((NewNode = (LISTNODE*)malloc(sizeof(LISTNODE))) != NULL) {
+//		NEXT(NewNode) = *list;
+//		*list = NewNode;
+//	}
+//	return NewNode;
+//}
+//
+//STATUS ReadFile(LIST* list, char* filename)
+//{
+//	FILE* fp;
+//
+//	WORKER worker, * ptr;
+//	int aux;
+//
+//	if ((fp = fopen(filename, "r")) != NULL) {
+//		while (!feof(fp)) {
+//			fscanf(fp, "%[^\n];%[^\n];%f;%d;\n", worker.name, worker.department, &worker.paycheck, &aux);
+//			if ((ptr = (WORKER*)malloc(sizeof(WORKER))) != NULL && DefineBeginning(list, ptr) != NULL) {
+//				strcpy(ptr->name, worker.name);
+//				strcpy(ptr->department, worker.department);
+//				ptr->paycheck = worker.paycheck;
+//				if (aux) ptr->regular = TRUE;
+//				else ptr->regular = FALSE;
+//			}
+//			else return ERROR;
+//		}
+//		fclose(fp);
+//		return OK;
+//	}
+//	else return ERROR;
+//}
+//
+//void RemoveFirstNode(LIST* list)
+//{
+//	LISTNODE* node;
+//
+//	node = NEXT(*list);
+//	free(DATA(*list));
+//	free(*list);
+//	*list = node;
+//}
+//
+//void FreeList(LIST* list)
+//{
+//	while (*list != NULL) {
+//		RemoveFirstNode(list);
+//	}
+//}
+//
+//float TotalPaycheck(LIST list)
+//{
+//	float total = 0.0;
+//	
+//	while (list != NULL) {
+//		total = total + ((WORKER*)DATA(list))->paycheck;
+//		list = NEXT(list);
+//	}
+//
+//	return total;
+//}
+//
+//BOOLEAN TurnToRegular(LIST list)
+//{
+//	char* name[MAX];
+//
+//	printf("What's the name of the employee? \n");
+//	gets(name);
+//
+//	while (list != NULL && strcmp(((WORKER*)DATA(list))->name, name)) {
+//		list = NEXT(list);
+//	}
+//
+//	if (list != NULL && !strcmp(((WORKER*)DATA(list))->name)) {
+//		((WORKER*)DATA(list))->regular = TRUE;
+//	}
+//}
+//
+//void DeleteNonRegular(LIST* list)
+//{
+//	LIST listt = *list;
+//	LIST prev = NULL;
+//
+//	if (list == NULL)
+//		return FALSE;
+//
+//	if (!(prev = (NODE*)malloc(sizeof(NODE))))
+//		return FALSE;
+//
+//	while (listt != NULL && (((WORKER*)DATA(listt))->regular) == FALSE) {
+//		*list = NEXT(listt);
+//		free(listt);
+//		listt = *list;
+//	}
+//
+//	while (listt != NULL) {
+//		while (listt != NULL && (((WORKER*)DATA(listt))->regular) != FALSE) {
+//			prev = listt;
+//			listt = NEXT(listt);
+//		}
+//
+//		if (listt == NULL)
+//			return TRUE;
+//
+//		NEXT(prev) = NEXT(listt);
+//
+//		free(listt);
+//		listt = NEXT(prev);
+//	}
+//	return TRUE;
+//}
---
+++ Node.c
@@ -0,0 +1,173 @@
+//
+//
+//
+//
+//
+//
+//
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+///*
+//struct Node
+//{
+//	int Data;
+//	struct Node* next;
+//};
+//*/
+// 
+//// With this there's no need to write "struct Node", just type NODE instead
+//
+//typedef struct Node
+//{
+//	int Data;
+//	struct Node* next;
+//}NODE;
+//
+//
+//#define DATA(Node) ((Node)->Data); // instead of First -> Data = 1 type DATA (First) = 1, for example
+//#define NEXT(Node) ((Node)->next); // instead of First -> Next = Second type NEXT (First) = Second, for example
+//
+//typedef NODE* List; // instead of NODE* just type List
+//
+//void PrintList(struct Node* list); // with the new redefined parameters it would be "List list" or without the latter "NODE* list"
+//void DefineBeginning(struct Node** list, int value);
+//void DefineLastNumber(struct Node** list, int value);
+//void DeleteValue(struct Node** list, int value);
+//void DeleteList(struct Node** list);
+//int SizeOfList(struct Node list);
+//
+//
+//void main() {
+//
+//	struct Node* First = NULL; // or struct Node * First = NULL, * Second = NULL, * Third=NULL
+//	struct Node* Second = NULL;
+//	struct Node* Third = NULL;
+//
+//	First = (struct Node*)malloc(sizeof(struct Node));
+//	Second = (struct Node*)malloc(sizeof(struct Node));
+//	Third = (struct Node*)malloc(sizeof(struct Node));
+//
+//	First->Data = 1;
+//	First->next = Second;
+//	Second->Data = 2;
+//	Second->next = Third;
+//	Third->Data = 3;
+//	Third->next = NULL;
+//	
+//	// showing the list
+//	/*
+//	struct Node* temp = First;
+//	
+//	while (temp != NULL) 
+//	{
+//		printf("%d \n", temp->Data);
+//		temp = temp->next;
+//	}
+//	*/
+//	//PrintList(First);
+//	
+//	// adding a new number to the beginning of the list
+//	/*
+//	struct Node* newn = (struct Node*)malloc(sizeof(struct Node));
+//
+//	newn->Data = 4;
+//	newn->next = First;
+//	First = newn;
+//	*/
+//	//PrintList(First);
+//
+//	//DefineBeginning(&First, 3);
+//	//DefineLastNumber(&Third, 3);
+//	//DeleteValue(&First, 3);
+//	//DeleteValue(&First, 3);
+//	//DeleteValue(&First, 3);
+//	//PrintList(First);
+//	//DeleteList(&First);
+//
+//	PrintList(First);
+//}
+//
+//void PrintList(struct Node* list)
+//{
+//	if (list == NULL) {
+//		printf("Empty list \n");
+//		return;
+//	}
+//
+//	// it doesn't need a temporary value because the list is a copy, in this case, of "First"
+//
+//	while (list != NULL) {
+//		printf("%d \n", list->Data);
+//		list = list->next;
+//	}
+//}
+//
+//void DefineBeginning(struct Node** list, int value) 
+//{
+//	struct Node* newn = (struct Node*)malloc(sizeof(struct Node));
+//	newn->Data = value;
+//	newn->next = *list; // same as "First" used before but instead has to be used with a pointer
+//	*list = newn;
+//}
+//
+//void DefineLastNumber(struct Node** list, int value)
+//{
+//	struct Node* newn = (struct Node*)malloc(sizeof(struct Node));
+//	struct Node* temp = *list; // it needs a temporary pointer so it finds the end of the list
+//	newn->Data = value;
+//	newn->next = NULL;
+//
+//	if (*list == NULL) {
+//		*list = newn;
+//		return;
+//	}
+//	// runs thru the list
+//	while (temp->next != NULL) {
+//		temp = temp->next;
+//	}
+//
+//	temp->next = newn;
+//}
+//
+//void DeleteValue(struct Node** list, int value)
+//{
+//	struct Node* prev = NULL, * temp;
+//
+//	temp = *list;
+//	if (temp != NULL && temp->Data == value) { // means it's the first element of the list
+//		*list = temp->next;
+//		free(temp);
+//		return;
+//	}
+//
+//	while (temp != NULL && temp->Data != value) {
+//		prev = temp;
+//		temp = temp->next;
+//	}
+//
+//	if (temp == NULL) // verify if it found any value
+//		return;
+//	// if it did find, do this next
+//	prev->next = temp->next;
+//	free(temp);
+//}
+//
+//void DeleteList(struct Node** list)
+//{
+//	struct Node* temp = *list;
+//	struct Node* nextt;
+//
+//	while (temp != NULL) {
+//		nextt = temp->next;
+//		free(temp);
+//		temp = nextt;
+//	}
+//
+//	*list = NULL;
+//}
+//
+//int SizeOfList(struct Node list)
+//{
+//	
+//}
